01d2e7c12361878ac5552b1070b8e6fac5695254
Cong Phuoc Hoang <hoangphuocbk2.07@gmail.com>
2021-07-13 18:08:43 +0700

    update kong.yml configuration

--- app.py
+++ app.py
@@ -3,6 +3,10 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/chaio')
 def hello_world():
     return 'Hello, Docker!'
+
+@app.route('/greet')
+def greet_func():
+    return 'Hello, This is /greet !'
